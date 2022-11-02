4b1e2b59cef629c0a173c79831f16264199c9545
RAYT21 <90043295+RAYT21@users.noreply.github.com>
2022-11-02 11:54:52 +0300

    final

--- main.py
+++ main.py
@@ -10,51 +10,51 @@ class Window:
         self.result_output = None
         self.root = Tk()
         self.root.title('Программа проверки информационной безопасности')
-        self.root.geometry('600x400')
+        self.root.geometry('1000x600')
         self.root.resizable(True, True)
 
         self.internet = Button(self.root, command=self.internet_connection_click,
-                               text='Проверка наличия соединения с интернетом', font='Consolas')
+                               text='Проверка наличия соединения  \nс интернетом', width=35, bg="#b38dd9", font='Consolas')
         self.internet.place(x=20, y=10)
         self.internet_result = Label(self.root, text='Проверка не проводилась', font='Consolas')
-        self.internet_result.place(x=500, y=10)
+        self.internet_result.place(x=400, y=10)
 
         self.firewall_place = Button(self.root, command=self.firewall_install_click,
-                                     text='Проверка наличия установленного межсетевого экрана', font='Consolas')
-        self.firewall_place.place(x=20, y=50)
+                                     text='Проверка наличия установленного \nмежсетевого экрана', width=35, bg="#b38dd9", font='Consolas')
+        self.firewall_place.place(x=20, y=70)
         self.firewall_place_result = Label(self.root, text='Проверка не проводилась', font='Consolas')
-        self.firewall_place_result.place(x=500, y=50)
+        self.firewall_place_result.place(x=400, y=70)
 
         self.firewall_status = Button(self.root, command=self.firewall_work_click,
-                                      text='Проверка работоспособности межсетевого экрана', font='Consolas')
-        self.firewall_status.place(x=20, y=90)
+                                      text='Проверка работоспособности \nмежсетевого экрана', width=35, bg="#b38dd9", font='Consolas')
+        self.firewall_status.place(x=20, y=130)
         self.firewall_status_result = Label(self.root, text='Проверка не проводилась', font='Consolas')
-        self.firewall_status_result.place(x=500, y=90)
+        self.firewall_status_result.place(x=400, y=130)
 
         self.antivirus_place = Button(self.root, command=self.antivirus_install_click,
-                                      text='Проверка наличия установленного антивирусного ПО', font='Consolas')
-        self.antivirus_place.place(x=20, y=160)
+                                      text='Проверка наличия \nустановленного антивирусного ПО', width=35, bg="#b38dd9", font='Consolas')
+        self.antivirus_place.place(x=20, y=190)
         self.antivirus_place_result = Label(self.root, text='Проверка не проводилась', font='Consolas')
-        self.antivirus_place_result.place(x=500, y=160)
+        self.antivirus_place_result.place(x=400, y=190)
 
         self.antivirus_status = Button(self.root, command=self.antivirus_work_click,
-                                       text='Проверка работоспособности антивирусного ПО', font='Consolas')
-        self.antivirus_status.place(x=20, y=200)
+                                       text='Проверка работоспособности \nантивирусного ПО', width=35, bg="#b38dd9", font='Consolas')
+        self.antivirus_status.place(x=20, y=250)
         self.antivirus_status_result = Label(self.root, text='Проверка не проводилась', font='Consolas')
-        self.antivirus_status_result.place(x=500, y=200)
+        self.antivirus_status_result.place(x=400, y=250)
 
-        self.result_painting = Button(self.root, command=self.result_painting_click, text='Вывести результат',
+        self.result_painting = Button(self.root, command=self.result_painting_click, text='Вывести результат', bg="#4169e1",
                                       font='Consolas')
-        self.result_painting.place(x=350, y=260)
-        self.result_painting_result = Listbox(self.root, font='Consolas')
-        self.result_painting_result.place(x=20, y=260)
+        self.result_painting.place(x=20, y=330)
+        self.result_painting_result = Listbox(self.root, width=55, bd=10, font='Consolas')
+        self.result_painting_result.place(x=400, y=330)
 
-        self.result_file = Button(self.root, command=self.result_file_click, text='Вывести результат в файл',
+        self.result_file = Button(self.root, command=self.result_file_click, text='Вывести результат в файл', bg="#4169e1",
                                   font='Consolas')
-        self.result_file.place(x=350, y=300)
+        self.result_file.place(x=20, y=380)
 
-        self.exit = Button(self.root, command=self.exit_click, text='Выйти', font='Consolas')
-        self.exit.place(x=350, y=340)
+        self.exit = Button(self.root, command=self.exit_click, text='Выйти', bg="red", font='Consolas')
+        self.exit.place(x=20, y=520)
 
         self.root.mainloop()
 
@@ -63,11 +63,11 @@ class Window:
             sock = socket.create_connection(("www.google.com", 80))
             if sock is not None:
                 sock.close()
-            self.internet_result.config(text='Данный компьютер подключен к интернету!', fg='green')
+            self.internet_result.config(text='Данный компьютер подключен к интернету', fg='green')
             return
         except OSError:
             pass
-        self.internet_result.config(text='Данный компьютер не подключен к интернету!', fg='red')
+        self.internet_result.config(text='Данный компьютер не подключен к интернету', fg='red')
 
     def firewall_install_click(self):
         lst = os.popen('drweb-ctl -v').read()
@@ -78,9 +78,9 @@ class Window:
                 flag = True
                 break
         if (flag):
-            self.firewall_place_result.config(text='Межсетевой экран DrWeb установлен!', fg='green')
+            self.firewall_place_result.config(text='Межсетевой экран DrWeb установлен', fg='green')
         else:
-            self.firewall_place_result.config(text='Межсетевой экран DrWeb не установлен!', fg='red')
+            self.firewall_place_result.config(text='Межсетевой экран DrWeb не установлен', fg='red')
 
     def firewall_work_click(self):
         lst = os.popen('ps -aux | grep [d]rweb').read()
@@ -91,10 +91,10 @@ class Window:
                 flag = True
                 break
         if (flag):
-            self.firewall_status_result.config(text='Межсетевой экран DrWeb функционирует!', fg='green')
+            self.firewall_status_result.config(text='Межсетевой экран DrWeb функционирует', fg='green')
         else:
             self.firewall_status_result.config(
-                text='Межсетевой экран функционирует неверно, или не функционирует вовсе!', fg='red')
+                text='Межсетевой экран функционирует неверно, или не функционирует вовсе', fg='red')
 
     def antivirus_install_click(self):
         lst = os.popen('drweb-ctl -v').read()
@@ -105,9 +105,9 @@ class Window:
                 flag = True
                 break
         if (flag):
-            self.antivirus_place_result.config(text='Антивирус DrWeb установлен!', fg='green')
+            self.antivirus_place_result.config(text='Антивирус DrWeb установлен', fg='green')
         else:
-            self.antivirus_place_result.config(text='Антивирус DrWeb не установлен!', fg='red')
+            self.antivirus_place_result.config(text='Антивирус DrWeb не установлен', fg='red')
 
     def antivirus_work_click(self):
         lst = os.popen('ps -aux | grep [d]rweb').read()
@@ -118,10 +118,10 @@ class Window:
                 flag = True
                 break
         if (flag):
-            self.antivirus_status_result.config(text='Монитор DrWeb функционирует!', fg='green')
+            self.antivirus_status_result.config(text='Монитор DrWeb функционирует', fg='green')
         else:
             self.antivirus_status_result.config(
-                text='Монитор DrWeb не функционирует, или функционирует неправильно!', fg='red')
+                text='Монитор DrWeb не функционирует, или функционирует неправильно', fg='red')
 
     def result_painting_click(self):
         self.result_output = ''
@@ -130,7 +130,7 @@ class Window:
         if (self.internet_result['text'] == 'Проверка не проводилась'):
             self.result_output += "1. Тестирование подключения к интернету не проводилось\n"
         else:
-            if (self.internet_result['text'] == 'Данный компьютер подключен к интернету!'):
+            if (self.internet_result['text'] == 'Данный компьютер подключен к интернету'):
                 self.result_output += "1. Данный компьютер подключен к интернету\n"
             else:
                 self.result_output += "1. Данный компьютер не подключен к интернету\n"
@@ -144,10 +144,10 @@ class Window:
                 if (self.firewall_status_result['text'] == 'Тестирование не проводилось'):
                     self.result_output += "но тестирование его работоспособности не проводилось\n"
                 else:
-                    if (self.firewall_status_result['text'] == 'Межсетевой экран DrWeb функционирует!'):
+                    if (self.firewall_status_result['text'] == 'Межсетевой экран DrWeb функционирует'):
                         self.result_output += "верно функционирует\n"
                     else:
-                        self.result_output += "но функционирует неверно, или вообще не фукционирует\n"
+                        self.result_output += "неверно или вообще не фукционирует\n"
             else:
                 self.result_output += "2. Межсетевой экран DrWeb не установлен\n"
 
@@ -155,12 +155,12 @@ class Window:
         if (self.antivirus_place_result['text'] == 'Проверка не проводилась'):
             self.result_output += "3. Тестирование антивирусного ПО не проводилось\n"
         else:
-            if (self.antivirus_place_result['text'] == 'Антивирус DrWeb установлен!'):
+            if (self.antivirus_place_result['text'] == 'Антивирус DrWeb установлен'):
                 self.result_output += "3. Антивирус DrWeb установлен, "
                 if (self.antivirus_status_result['text'] == 'Тестирование не проводилось'):
                     self.result_output += "но тестирование работоспособности монитора не проводилось\n"
                 else:
-                    if (self.antivirus_status_result['text'] == 'Монитор DrWeb функционирует!'):
+                    if (self.antivirus_status_result['text'] == 'Монитор DrWeb функционирует'):
                         self.result_output += "монитор верно функционирует\n"
                     else:
                         self.result_output += "но монитор функционирует неверно, или вообще не фукционирует\n"
